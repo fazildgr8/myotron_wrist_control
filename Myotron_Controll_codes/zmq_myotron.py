34b513cae00cf5d17d14c10d564935f9bf8ccf5e
fazildgr8 <mohamedfazilsulaiman@gmail.com>
2021-02-19 12:20:27 -0500

    Control Updates

---
+++ Myotron_Controll_codes/zmq_myotron.py
@@ -0,0 +1,24 @@
+import zmq
+import datetime
+import random
+import numpy as np
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://127.0.0.1:5555")
+# socket.bind("tcp://localhost:5555")
+
+def arr_fromat(arr):
+    string = str(arr[0]);
+    arr2 = arr[1:]
+    for e in arr2:
+        string = string+' '+str(e)
+    return string
+def send_qpos(qpos):
+    socket.send_string(arr_fromat(qpos))
+
+# while True:
+#     n = random.uniform(-3.14,3.14)
+#     qpos = n*np.ones(13)
+#     print(qpos)
+#     socket.send_string(arr_fromat(qpos))
