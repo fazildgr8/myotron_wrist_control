78df17393402e8a4f7aeef27562d04e6cc0595f8
fazildgr8 <mohamedfazilsulaiman@gmail.com>
2021-03-10 20:52:07 -0500

    Emg testing update

--- Myotron_Controll_codes/emg_sim_time_sync.py
+++ Myotron_Controll_codes/emg_sim_time_sync.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from threading import Thread
 from keras.models import load_model
 
-global prosup_clf, emg_window, curr_ts
+global prosup_clf, emg_window, curr_ts, curr_clf
 
 
 win_len = 250
@@ -19,12 +19,13 @@ prosup_model = load_model(prosup_file)
 
 emg_labels = ['emg1','emg2','emg3','emg4','emg5','emg6','emg7','emg8']
 
-emg_df = pd.read_excel('F:/The Stuffs/Awear/Final_Project/myotron_control/Myotron_Controll_codes/sim_data/prosup_norm_200s.xlsx')
-emg_df.columns = ['t[s]']+emg_labels
+emg_df = pd.read_excel('F:/The Stuffs/Awear/Final_Project/myotron_control/Myotron_Controll_codes/sim_data/prosup_norm_200s_labeled.xlsx')
+# emg_df.columns = ['t[s]']+emg_labels
 print(emg_df.columns)
 
 emg_data = np.array(emg_df[emg_labels])
-emg_ts = np.array(emg_df['t[s]'])
+# emg_ts = np.array(emg_df['t[s]'])
+emg_True_labels = np.array(emg_df['labels'])
 
 def classify(data,model):
     data = np.reshape(data,(1,win_len,n_channels))
@@ -33,18 +34,19 @@ def classify(data,model):
     return clf
  
 def read_emg_window():
-    global emg_window, curr_ts
+    global emg_window, curr_ts, curr_clf, emg_window
     i = 0
     while True:
         emg_window = emg_data[i:i+win_len]
-        curr_ts = emg_ts[i]
-        sleep(1/2000)
+        curr_ts = i
+        curr_clf = emg_True_labels[i]
+        # sleep(1/2000)
         i = i+1
 def classify_loop():
     global prosup_clf, emg_window
     while True:
         prosup_clf = classify(emg_window,prosup_model)
-
+        # print(curr_ts,' ',prosup_clf,' ',curr_clf)
 
 if __name__ == '__main__':
     thread_emg_read = Thread(target=read_emg_window)
@@ -57,6 +59,6 @@ if __name__ == '__main__':
         now_time = datetime.now()
         elp = now_time - start_time
         elapsed = elp.total_seconds()
-        print(elapsed,' ',curr_ts,' ',prosup_clf)
+        print(curr_ts,' ',prosup_clf,' ',curr_clf)
         if elapsed>10000:
             break
